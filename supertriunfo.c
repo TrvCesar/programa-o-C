e859fc95ef1e976e91a392fb9d9e60a4e7bb058d
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-02-22 22:13:38 +0000

    criei printf

---
+++ supertriunfo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+char estado;
+char codigo[20];
+char nome[20];
+int populacao;
+float area;
+float pib;
+int ponto;
+
+printf("Escreva o Estado: \n");
+scanf(" %c,estado");
+
+
+
+
+
+}
