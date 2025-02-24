83ed986519f3255a995204fa94d9d13542d9dc6f
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-02-24 00:55:17 +0000

    criei printf

--- estruturas de decisões/teste1.c
+++ estruturas de decisões/teste1.c
@@ -2,14 +2,16 @@
 
 int main(){
 
-int numero1 = 8;
+int numero1 = 3;
 int numero2 = 5;
 
 if (numero1 > numero2 ) { 
 printf("numero 1 é maior que numero 2\n");
 
 }
-
+else{
+    printf("numero 1 é menor que numero 2\n");
+}
 
 
 
