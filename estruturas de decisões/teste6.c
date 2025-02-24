f384a4911ff73d348d329f9592d62a332092280f
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-02-24 00:59:32 +0000

    criei printf

--- estruturas de decisões/teste6.c
+++ estruturas de decisões/teste6.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int numero = 4;
+  int numero = 5;
 
   if (numero % 2 == 0) {
     printf("O número é par\n");
   }
-
+else{
+    printf("O numero é impar");
+}
   return 0;
 }
