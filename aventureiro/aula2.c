b2d7e7e47dc4f03c1be5fb8920cd5625e60d37a0
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-03-01 16:53:52 +0000

    inclui printf

---
+++ aventureiro/aula2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+/*
+soma (+)
+subtração (-)
+multiplicação (*)
+divisão (/)
+*/
+int numero1, numero2;
+int soma, subtracao, multiplicacao, divisao;
+
+soma = numero1 + numero2;
+
+
+
+
+
+
+
+}
