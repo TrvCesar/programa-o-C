e612e522edcb18c75eee98d65dca3ca24899fa40
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-03-05 10:41:49 +0000

    inclui commit

--- supertrunfo2.c
+++ supertrunfo2.c
@@ -9,8 +9,9 @@ float area;
 float pib;
 int ponto;
 
-printf("Super Trunfo\n");
-printf("Cadastro Carta 1\n");
+
+printf("Carta Super Trunfo\n");
+printf("Carta 1 \n");
 printf("Escreva o Estado: \n");
 scanf(" %c",&estado);
 printf("Escreva o Código: \n");
@@ -29,18 +30,18 @@ scanf(" %d",&ponto);
 float densidade = (float)  populacao / area;
 float percapita = (float) pib / populacao;
 
-printf("Carta 1\n")
+printf("Carta 1\n");
 printf("Estado: %c\n", estado);
 printf("Código: %s\n",codigo);
 printf("Cidade: %s\n",nome);
 printf("População: %d\n",populacao);
-printf("Area: %f\n",area);
-printf("PIB: %f\n",pib);
-printf("Pontos Turisticos: %d",ponto);
+printf("Area: %.2f\n",area);
+printf("PIB: %.2f\n",pib);
+printf("Pontos Turisticos: %d\n",ponto);
 printf("Densidade Populacional: %.2f\n",densidade);
-printf("PIB per capita: %.2f\n",percapita);
+printf("Pib per capita: %.2f\n",percapita);
 
-printf("Cadastro Carta 2\n");
+printf("Carta 2\n");
 printf("Escreva o Estado: \n");
 scanf(" %c",&estado);
 printf("Escreva o Código: \n");
@@ -56,16 +57,22 @@ scanf(" %f",&pib);
 printf("Quantos pontos turisticos: \n");
 scanf(" %d",&ponto);
 
+
 printf("Carta 2\n");
 printf("Estado: %c\n", estado);
 printf("Código: %s\n",codigo);
 printf("Cidade: %s\n",nome);
 printf("População: %d\n",populacao);
-printf("Area: %f\n",area);
-printf("PIB: %f\n",pib);
-printf("Pontos Turisticos: %d",ponto);
+printf("Area: %.2f\n",area);
+printf("PIB: %.2f\n",pib);
+printf("Pontos Turisticos: %d\n",ponto);
 printf("Densidade Populacional: %.2f\n",densidade);
-printf("PIB per capita: %.2f\n",percapita);
+printf("Pib per capita: %.2f\n",percapita);
+
+
+
+
+
 
 
 
