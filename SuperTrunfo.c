cb34c402e12f7fd2e54d0c25bef032275fcd3692
TrvCesar <109994323+TrvCesar@users.noreply.github.com>
2025-03-17 00:20:01 +0000

    add commit

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-// 
+// entrada das variaveis.
 char estado;
 char codigo [20];
 char nome [20];
@@ -15,6 +15,7 @@ float pib2;
 int ponto1;
 int ponto2;
 
+// Entrada de dados carta 1
 printf("Carta Super Trunfo\n");
 printf("Carta 1 \n");
 printf("Escreva o Estado: \n");
@@ -32,11 +33,11 @@ scanf(" %f",&pib1);
 printf("Quantos pontos turisticos: \n");
 scanf(" %d",&ponto1);
 
-
+// calculo densidade e per capita da carta 1.
 float densidade1 = (float) populacao1 / area1;
 float percapita1 = (float) pib1 / populacao1;
 
-
+// Saída carta 1.
 printf("Carta 1\n");
 printf("Estado: %c\n", estado);
 printf("Código: %s\n",codigo);
@@ -48,7 +49,7 @@ printf("Pontos Turisticos: %d\n",ponto1);
 printf("Densidade Populacional: %.2f\n",densidade1);
 printf("Pib per capita: %.2f\n",percapita1);
 
-
+// Entrada carta 2.
 printf("Carta Super Trunfo\n");
 printf("Carta 2 \n");
 printf("Escreva o Estado: \n");
@@ -66,9 +67,11 @@ scanf(" %f",&pib2);
 printf("Quantos pontos turisticos: \n");
 scanf(" %d",&ponto2);
 
+// calculo densidade e per capita carta 2.
 float densidade2 = (float) populacao2 / area2;
 float percapita2 = (float) pib2 / populacao2;
 
+// Saída Carta 2.
 printf("Carta 2\n");
 printf("Estado: %c\n", estado);
 printf("Código: %s\n",codigo);
@@ -80,6 +83,7 @@ printf("Pontos Turisticos: %d\n",ponto2);
 printf("Densidade Populacional: %.2f\n",densidade2);
 printf("Pib per capita: %.2f\n",percapita2);
 
+// Comparação e Escolha da carta vencedora.
 if(populacao1>populacao2){
     printf("Carta 1 Venceu,População: %d\n",populacao1);
 }else{
